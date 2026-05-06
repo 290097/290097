4fd99916675a2c449c23a13934655a0a8d0703d0
290097 <290097@student.pwr.edu.pl>
2026-05-06 19:35:53 +0200

    Add files via upload

---
+++ lab4_ppo.cpp
@@ -0,0 +1,255 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Osoba {
+protected:
+    string imie;
+    string nazwisko;
+
+public:
+    Osoba() : imie(""), nazwisko("") {}
+    virtual ~Osoba() {}
+
+   virtual bool ustawDane(string i, string n) {
+        if (i.length() < 3 || n.length() < 3) {
+            return false;
+        }
+        imie = i;
+        nazwisko = n;
+        return true;
+    }
+
+    string getNazwisko() { return nazwisko; }
+
+    virtual void drukuj() {
+        cout << imie << " " << nazwisko;
+    }
+};
+
+class Student : public Osoba {
+private:
+    int indeks;
+public:
+    Student() : Osoba(), indeks(0) {}
+
+    void ustawIndeks(int ind) { indeks = ind; }
+
+    void drukuj() override { 
+        Osoba::drukuj();
+        cout << " [Student, Indeks: " << indeks << "]";
+    }
+};
+
+class Starosta : public Osoba {
+private:
+    int id_pracownika;
+public:
+    Starosta() : Osoba(), id_pracownika(0) {}
+
+    void ustawID(int id) { id_pracownika = id; }
+
+    void drukuj() override {
+        Osoba::drukuj();
+        cout << " [Starosta, ID: " << id_pracownika << "]";
+    }
+};
+
+
+
+class ListaObecnosci {
+private:
+    Osoba** tabOsob;
+    bool* obecnosc;
+    int ilosc;
+    int maxOsob;
+
+public:
+    ListaObecnosci() : ilosc(0), maxOsob(10) {
+        tabOsob = new Osoba * [maxOsob];
+        obecnosc = new bool[maxOsob];
+    }
+
+    void dodajDoListy(Osoba* os) {
+        if (ilosc < maxOsob) {
+            tabOsob[ilosc] = os;
+            obecnosc[ilosc] = false;
+            ilosc++;
+        }
+    }
+
+    void zmienStatus(string nazwisko, bool stan) {
+        for (int i = 0; i < ilosc; i++) {
+            if (tabOsob[i]->getNazwisko() == nazwisko) {
+                obecnosc[i] = stan;
+                cout << "Zmieniono status obecnosci dla: " << nazwisko << endl;
+                return;
+            }
+        }
+        cout << "Nie znaleziono takiej osoby na tej liscie!" << endl;
+    }
+
+    void drukuj() {
+        if (ilosc == 0) cout << "(lista pusta)" << endl;
+        for (int i = 0; i < ilosc; i++) {
+            tabOsob[i]->drukuj();
+            cout << " -> " << (obecnosc[i] ? "OBECNY" : "NIEOBECNY") << endl;
+        }
+    }
+};
+
+class InterfejsUzytkownika {
+private:
+    Osoba** tablicaOsob;
+    int aktualnaIloscOsob;
+    int maxOsob;
+
+    ListaObecnosci* tablicaList;
+    int aktualnaIloscList;
+    int maxList;
+
+public:
+    InterfejsUzytkownika(Osoba** tO, int nO, ListaObecnosci* tL, int nL)
+        : tablicaOsob(tO), maxOsob(nO), aktualnaIloscOsob(0),
+        tablicaList(tL), maxList(nL), aktualnaIloscList(0) {
+    }
+
+    void loop() {
+        int wybor;
+        do {
+            cout << "1. Dodaj osobe" << endl;
+            cout << "2. Dodaj liste obecnosci" << endl;
+            cout << "3. Dodaj osobe do listy" << endl;
+            cout << "4. Ustaw obecnosc osoby na liscie" << endl;
+            cout << "5. Usun osobe (z bazy)" << endl;
+            cout << "6. Zmien dane osoby" << endl;
+            cout << "7. Podglad bazy i list" << endl;
+            cout << "0. Wyjscie" << endl;
+            cout << "Wybor: "; cin >> wybor;
+
+            switch (wybor) {
+            case 1: {
+                if (aktualnaIloscOsob < maxOsob) {
+                    string im, naz; int ind;
+                    bool sukces = false;
+                    do {
+                        cout << "Imie: "; cin >> im;
+                        cout << "Nazwisko: "; cin >> naz;
+                        cout << "Indeks: "; cin >> ind;
+                        Student* nowy = new Student();
+                        if (nowy->ustawDane(im, naz)) { 
+                            nowy->ustawIndeks(ind);
+                            tablicaOsob[aktualnaIloscOsob] = nowy;
+                            sukces = true;
+                        }
+                        else {
+                            delete nowy; 
+                            sukces = false;
+                        }
+                        if (!sukces) {
+                            cout << "Blad: imie lub nazwisko nie maja 3 znakow" << endl;
+                        }
+                    } while (!sukces);
+
+                    aktualnaIloscOsob++;
+                }
+                else {
+                    cout << "Baza pelna!" << endl;
+                }
+                break;
+            }
+
+            case 2: {
+                if (aktualnaIloscList < maxList) {
+                    aktualnaIloscList++;
+                    cout << "Aktywowano liste nr " << aktualnaIloscList << endl;
+                }
+                else cout << "Limit list wyczerpany!" << endl;
+                break;
+            }
+            case 3: {
+                int nrO, nrL;
+                cout << "Nr osoby (1-" << aktualnaIloscOsob << "): "; cin >> nrO;
+                cout << "Nr listy (1-" << aktualnaIloscList << "): "; cin >> nrL;
+                if (nrO > 0 && nrO <= aktualnaIloscOsob && nrL > 0 && nrL <= aktualnaIloscList) {
+                    tablicaList[nrL - 1].dodajDoListy(tablicaOsob[nrO - 1]);
+                }
+                break;
+            }
+            case 4: {
+                int nrL; string naz; int stan;
+                cout << "Nr listy: "; cin >> nrL;
+                cout << "Nazwisko osoby: "; cin >> naz;
+                cout << "Obecny? (1-Tak, 0-Nie): "; cin >> stan;
+                if (nrL > 0 && nrL <= aktualnaIloscList) {
+                    tablicaList[nrL - 1].zmienStatus(naz, stan == 1);
+                }
+                break;
+            }
+            case 5: {
+                string naz;
+                cout << "Nazwisko osoby do usuniecia: "; cin >> naz;
+                for (int i = 0; i < aktualnaIloscOsob; i++) {
+                    if (tablicaOsob[i]->getNazwisko() == naz) {
+                        for (int j = i; j < aktualnaIloscOsob - 1; j++) {
+                            tablicaOsob[j] = tablicaOsob[j + 1];
+                        }
+                        aktualnaIloscOsob--;
+                        cout << "Osoba usunieta" << endl;
+                        break;
+                    }
+                }
+                break;
+            }
+            case 6: {
+           
+                string klucz;
+                cout << "Podaj nazwisko osoby do edycji: "; cin >> klucz;
+                for (int i = 0; i < aktualnaIloscOsob; i++) {
+                    if (tablicaOsob[i]->getNazwisko() == klucz) {
+                        string ni, nn; int nind;
+                        bool sukces = false;
+                        do {
+                            cout << "Nowe imie (min. 3 znaki): "; cin >> ni;
+                            cout << "Nowe nazwisko (min. 3 znaki): "; cin >> nn;
+                            cout << "Nowy indeks: "; cin >> nind;
+
+                            sukces = tablicaOsob[i]->ustawDane(ni, nn);
+
+                            if (sukces) {
+                            
+                                Student* s = (Student*)tablicaOsob[i];
+                                s->ustawIndeks(nind);
+
+                                cout << "Dane zaktualizowane!" << endl;
+                            }
+                            else {
+                                cout << "Blad: Za krotkie dane!" << endl;
+                            }
+                        } while (!sukces);
+                        break;
+                    }
+                }
+                break;
+            
+            }
+            case 7: {
+                for (int i = 0; i < aktualnaIloscList; i++) {
+                    cout << "\nLISTA " << i + 1 << ":" << endl;
+                    tablicaList[i].drukuj();
+                }
+                break;
+            }
+            }
+        } while (wybor != 0);
+    }
+};
+
+int main() {
+    Osoba** tabO = new Osoba*[10];
+    ListaObecnosci tabL[2];
+    InterfejsUzytkownika ui(tabO, 10, tabL, 2);
+    ui.loop();
+    return 0;
+}
